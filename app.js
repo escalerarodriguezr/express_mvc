2eab17270b1244d08c409582a5ea1cff64e68a8e
Rafael Escalera <escalerarodriguezr@gmail.com>
2023-02-05 22:26:00 +0100

    Inital commit

---
+++ app.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express')
+const hbs = require('hbs');
+
+const app = express()
+const port = process.env.PORT;
+
+// Handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials( __dirname + '/views/partials');
+
+// Servir contenido estático
+app.use( express.static('public') );
+
+app.get('/', (req, res) => {
+    res.render('home',{
+        title: 'Default..'
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
